19688c9ea673a7c66609406f6882988a92d1725e
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-07-05 08:48:22 +0300

    Create user queries and resolve them

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,14 @@
 import { users } from "./db";
 
-
 const resolvers = {
-    Query: {
-        hello: () => "Hello World!",
+  Query: {
+    user: (parent, { id }, context, info) => {
+      return users.find(user => user.id === parseInt(id));
     },
-}
+    users: (parent, args, context, info) => {
+      return users;
+    }
+  }
+};
 
 export default resolvers;
